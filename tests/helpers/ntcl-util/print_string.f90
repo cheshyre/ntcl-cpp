1ea2843c4b1dfcc7f5f0988453b12df09ec9d532
Matthias Heinz <heinz.matthias.3@gmail.com>
2022-04-22 17:24:01 +0200

    Add helpers

--- tests/helpers/ntcl-util/print_string.f90
+++ tests/helpers/ntcl-util/print_string.f90
@@ -5,10 +5,15 @@ module print_string_module
     private
 
     public :: print_string
+    public :: print_string_basic
 
     interface print_string
         module procedure print_string
     end interface print_string
+
+    interface print_string_basic
+        module procedure print_string_basic
+    end interface print_string_basic
 
 contains
 
@@ -19,5 +24,11 @@ contains
         print *, "char_array=", str%as_char_array()
         print *, "length", str%length
     end subroutine print_string
+
+    subroutine print_string_basic(str)
+        type(string), intent(in) :: str
+
+        print *, str%as_char_array()
+    end subroutine print_string_basic
 
 end module print_string_module
