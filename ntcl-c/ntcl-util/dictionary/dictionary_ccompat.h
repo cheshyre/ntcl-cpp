604ec70e49e1c836fad2fdbb6ddcedc073bc7450
Matthias Heinz <heinz.matthias.3@gmail.com>
2022-04-22 17:23:27 +0200

    Fix mistakes in header

--- ntcl-c/ntcl-util/dictionary/dictionary_ccompat.h
+++ ntcl-c/ntcl-util/dictionary/dictionary_ccompat.h
@@ -10,7 +10,7 @@ extern "C" {
 
 struct dictionary_ccompat {
   void* ptr_to_class;
-}
+};
 
 void create_dictionary_ccompat(struct dictionary_ccompat* dict_handle_ptr);
 
@@ -18,8 +18,8 @@ void cleanup_dictionary_ccompat(struct dictionary_ccompat* dict_handle_ptr);
 
 void add_key_value_pair_to_dictionary_ccompat(
     struct dictionary_ccompat* dict_handle_ptr,
-    struct short_string_ccompat* key_ptr,
-    struct short_string_ccompat* value_ptr);
+    struct ntcl_short_string_ccompat* key_ptr,
+    struct ntcl_short_string_ccompat* value_ptr);
 
 #ifdef __cplusplus
 }
